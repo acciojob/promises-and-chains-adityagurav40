f08dc86dcdb91686a5bc2742d3e6d94d925878a7
adityagurav40 <113020833+adityagurav40@users.noreply.github.com>
2023-04-03 17:12:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#name');
+const ageInput = document.querySelector('#age');
+const btn = document.querySelector('#btn');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const name = nameInput.value;
+  const age = ageInput.value;
+  
+  if (!name || !age) {
+    alert('Please enter both your name and age.');
+    return;
+  }
+  
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age >= 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+  
+  promise
+    .then((message) => alert(message))
+    .catch((error) => alert(error));
+});
